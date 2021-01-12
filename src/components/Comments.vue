e6916fa28dc248312694580983c45cbd74c680fa
Joao Rangel <joao.vran@gmail.com>
2021-01-11 22:56:25 -0300

    separate FormTodo in new component

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,69 +1,50 @@
 <template>
-      <div class="container">
-        <h1>Comentários</h1>
-        <hr />
-        <div class="form-todo form-group">
-          <p>
-            <input type="text" placeholder="nome" name="author" class="form-control" v-model="name">
-          </p>
-          <p>
-            <textarea placeholder="Comentário" name="message" class="form-control" v-model="message"></textarea>
-          </p>
-          <button v-on:click="addComment" class="btn btn-primary">Comentar</button>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr />
+     <FormTodo v-on:add-todo="addComment" />
+    <hr />
+    <div class="list-group">
+      <div class="list-group-item" :key="index" v-for="(comment, index) in allComments">
+        <span class="comment__author">
+          Autor: <strong>{{comment.name}}</strong>
+        </span>
+        <p>{{comment.message}}</p>
+        <div>
+          <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)" >Excluir</a>
         </div>
-        <hr />
-        <div class="list-group">
-          <div class="list-group-item" :key="index" v-for="(comment, index) in allComments">
-            <span class="comment__author">
-              Autor: <strong>{{comment.name}}</strong>
-            </span>
-            <p>{{comment.message}}</p>
-            <div>
-              <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)" >Excluir</a>
-            </div>
-          </div>
-        </div>
-        <hr />
-      </div>  
+      </div>
+    </div>
+    <hr />
+  </div>  
 </template>
 
 <script>
-export default {
-   data() {
-        return {
-          comments: [
-            {
-              name: 'João',
-              message: 'Agora vai em!'
-            },
-          ],
-          name: '',
-          message: ''
-        }
-      },
-      methods: {
-        addComment() {
-          if (this.message.trim() === '') return;
-
-          this.comments.push({
-            name: this.name,
-            message: this.message
-          });
-
-          this.name = '';
-          this.message = '';
-        },
-        removeComment(index) {
-          this.comments.splice(index, 1)
-        }
-      },
-      computed: {
-        allComments() {
-          return this.comments.map(comment => ({
-            ...comment,
-            name: comment.name.trim() === '' ? 'Anônimo' : comment.name
-          }))
-        }
+  import FormTodo from './FormTodo.vue'
+  export default {
+    components: {
+      FormTodo
+    },
+    data() {
+      return {
+        comments: [],
       }
-}
+    },
+    methods: {
+      addComment(comment) {
+        this.comments.push(comment);
+      },
+      removeComment(index) {
+        this.comments.splice(index, 1)
+      }
+    },
+    computed: {
+      allComments() {
+        return this.comments.map(comment => ({
+          ...comment,
+          name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+        }))
+      }
+    }
+  }
 </script>
